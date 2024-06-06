7875d43ae0d2541d17519844bcaee520d5970d77
Alexander Peasey <alexander.peasey@gmail.com>
2024-06-06 11:32:56 +0200

    Fetching data from JSON Server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './app.scss'
+import { useEffect, useState } from "react";
+import "./app.scss";
 
-function App() {
-  
-
-  return (
-   <div className="hello">Hello</div>
-  )
+interface Quest {
+  name: string;
+  id: string;
 }
 
-export default App
+const App: React.FC = () => {
+  const [quests, setQuests] = useState<Quest[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/quests").then((response) => response.json().then((data) => setQuests(data)));
+  });
+  console.log(quests)
+  return (
+    <div>
+      <ul>
+        {quests.map((quest) => (
+          <li key={quest.id}>{quest.id}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
